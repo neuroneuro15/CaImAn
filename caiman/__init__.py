51a80c744060e811006d493985d9423737901c5d
nickdg <delgrosso@bio.lmu.de>
2017-11-20 00:01:31 +0100

    Tiny rearrangement of import order.

--- caiman/__init__.py
+++ caiman/__init__.py
@@ -1,7 +1,8 @@
 from . import io
+from .movie import Movie
 from .cluster import start_server,stop_server
 from .mmapping import load_memmap,save_memmap,save_memmap_each,save_memmap_join
 from .summary_images import local_correlations
-from .movie import Movie
+
 
 #from .source_extraction import cnmf
