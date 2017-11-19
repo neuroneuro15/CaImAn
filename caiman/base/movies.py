8b0874b2ebcdf4cc186af917904b111fb841d3bd
nickdg <delgrosso@bio.lmu.de>
2017-11-19 17:54:29 +0100

    moved the two implemented trace methods to Movie class (adding *_trace to the name for compatibility and note reasons) to simplify dependency tree.

--- caiman/base/movies.py
+++ caiman/base/movies.py
@@ -530,6 +530,46 @@ class Movie(np.ndarray):
         print(('Final Size Movie:' +  np.str(self.shape)))
         return mov_out, Movie(movBL, fr=self.fr, start_time=self.start_time, meta_data=self.meta_data, file_name=self.file_name)
 
+    def computeDFF_trace(self,window_sec=5,minQuantile=20):
+        """
+        compute the DFF of the Movie
+
+        In order to compute the baseline frames are binned according to the window length parameter
+        and then the intermediate values are interpolated.
+
+        Parameters:
+        ----------
+        secsWindow: length of the windows used to compute the quantile
+
+        quantilMin : value of the quantile
+
+        Raise:
+        -----
+        ValueError("All traces must be positive")
+
+        ValueError("The window must be shorter than the total length")
+        """
+        if np.min(self)<=0:
+            raise ValueError("All traces must be positive")
+
+        T,num_neurons=self.shape
+        window=int(window_sec*self.fr)
+        print(window)
+        if window >= T:
+            raise ValueError("The window must be shorter than the total length")
+
+        tracesDFF=[]
+        for tr in self.T:
+            print((tr.shape))
+            traceBL=[np.percentile(tr[i:i+window],minQuantile) for i in range(1,len(tr)-window)]
+            missing=np.percentile(tr[-window:],minQuantile);
+            missing=np.repeat(missing,window+1)
+            traceBL=np.concatenate((traceBL,missing))
+            tracesDFF.append(old_div((tr-traceBL),traceBL))
+
+        return self.__class__(np.asarray(tracesDFF).T,**self.__dict__)
+
+
     def NonnegativeMatrixFactorization(self,n_components=30, init='nndsvd', beta=1,tol=5e-7, sparseness='components',**kwargs):
         """
         See documentation for scikit-learn NMF
@@ -999,6 +1039,58 @@ class Movie(np.ndarray):
         T,_,_=self.shape
         return Movie(np.concatenate([self[j:j + window, :, :].local_correlations(
             eight_neighbours=True)[np.newaxis, :, :] for j in range(T-window)], axis=0), fr=self.fr)
+
+
+    def plot_trace(self, stacked=True, subtract_minimum=False, cmap=pl.cm.jet, **kwargs):
+        """Plot the data as a trace (Note: experimental method, may not work quite right yet.)
+
+        author: ben deverett
+
+        Parameters:
+        ----------
+        stacked : bool
+            for multiple columns of data, stack instead of overlaying
+
+        subtract_minimum : bool
+            subtract minimum from each individual trace
+
+        cmap : matplotlib.LinearSegmentedColormap
+            color map for display. Options are found in pl.colormaps(), and are accessed as pl.cm.my_favourite_map
+
+        kwargs : dict
+            any arguments accepted by matplotlib.plot
+
+        Returns:
+        -------
+        The matplotlib axes object corresponding to the data plot
+        """
+        d = self.copy()
+        n = 1 #number of traces
+        if len(d.shape)>1:
+            n = d.shape[1]
+
+        ax = pl.gca()
+
+        colors = cmap(np.linspace(0, 1, n))
+        ax.set_color_cycle(colors)
+
+        if subtract_minimum:
+            d -= d.min(axis=0)
+        if stacked and n>1:
+            d += np.append(0, np.cumsum(d.max(axis=0))[:-1])
+        ax.plot(self.time, d, **kwargs)
+
+        # display trace labels along right
+        ax2 = ax.twinx()
+        ax2.set_ylim(ax.get_ylim())
+        ax2.set_yticks(np.atleast_1d(d.mean(axis=0)))
+        ax2.set_yticklabels([str(i) for i in range(n)], weight='bold')
+        [l.set_color(c) for l,c in zip(ax2.get_yticklabels(), colors)]
+
+        pl.gcf().canvas.draw()
+        return ax
+
+
 
     def play(self,gain=1,fr=None,magnification=1,offset=0,interpolation=cv2.INTER_LINEAR,
              backend='opencv',do_loop=False, bord_px = None):
