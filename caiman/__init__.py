76c0ec86794fd141782ed5de9907e0ebef202899
nickdg <delgrosso@bio.lmu.de>
2017-12-02 06:32:07 +0100

    import paralel_dot_product()

--- caiman/__init__.py
+++ caiman/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
-from caiman.io.mmapping import load_memmap,save_memmap,save_memmap_each,save_memmap_join
+from caiman.io.mmapping import load_memmap, save_memmap, save_memmap_each, save_memmap_join
 from . import io
-from .cluster import start_server,stop_server
+from .cluster import start_server, stop_server
 from .movie import Movie
 from .patches import get_patches_from_image, extract_patch_coordinates, apply_to_patch, function_place_holder
 from .summary_images import local_correlations
 from .behavior import select_roi, extract_motor_components_OF, extract_magnitude_and_angle_from_OF, compute_optical_flow
+from .parallel import parallel_dot_product
 
 #from .source_extraction import cnmf
