6e0563eda792f193befecd7926a1947b9a37b06d
nickdg <delgrosso@bio.lmu.de>
2017-12-02 05:59:21 +0100

    removed non-existing functions from import.

--- caiman/io/__init__.py
+++ caiman/io/__init__.py
@@ -1,3 +1,3 @@
 from .sbx import sbxloadmat, sbxread, sbxreadskip, sbxshape
 from .tiff import tifffile
-from .mmapping import load_memmap, save_memmap_each, save_memmap_join, save_memmap, parallel_dot_product, save_tif_to_mmap_online
+from .mmapping import load_memmap, save_memmap
